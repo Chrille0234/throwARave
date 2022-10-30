a7a72aac34731aca1156d70a238c6f13fba1c3d3
Chrille0234 <113505641+Chrille0234@users.noreply.github.com>
2022-10-30 23:17:42 +0100

    Add files via upload

---
+++ trigger.js
@@ -0,0 +1,8 @@
+function partyMode(){
+    var party = document.querySelector(".default");
+        party.classList.toggle("party");
+
+    var tekst = document.querySelector(".tekstDefault");
+        tekst.classList.toggle("tekst");
+
+}
