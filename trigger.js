08aa166c040f2ad59417bb07f6c570d48a302065
chrille0234 <madsen.chr@hotmail.com>
2023-01-17 08:28:03 +0100

    hm

--- trigger.js
+++ trigger.js
@@ -10,13 +10,9 @@ function alert(){
     alert("do not use if you're prone to seizures");
 }
 
-  // Get the button element by its ID
   var button = document.getElementById("play");
   
-  // Add an event listener to the button
   button.addEventListener("click", function() {
-    // If the music is currently playing, pause it and set the current playback position to 0
-    // Otherwise, play the music
     if (!music.paused) {
       music.currentTime = 0;
       music.pause();
@@ -25,16 +21,10 @@ function alert(){
     }
   });
   
-  // Create an audio element to hold the music
   var music = new Audio();
-  
-  // Set the source of the audio element to the URL of the music file
   music.src = "musik.mp3";
-  
-  // Enable looping on the audio element
   music.loop = true;
-  
-  // Load the music file
+
   music.load();
 
 
